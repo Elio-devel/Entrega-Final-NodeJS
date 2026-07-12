d6e440585cba4cff510fa681c5360528bc9e2c8f
Elio <eliocavalieri@gmail.com>
2026-07-12 19:27:41 -0300

    Actualizar proyecto

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import { generateToken } from "../data/token.js";
 
 export async function login(req, res) {
     const { email, password } = req.body;
-    if (email === "admin@gmail.com" && password === "123456") {
+    if (email === "admin@techlab.com" && password === "123456") {
         const token = generateToken({ id: 1, email: email });
         res.json({ token });
     } else {
